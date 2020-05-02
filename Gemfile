1236dadbdad5e17bb4738597b9968e560ac65485
schopenhauer <len@iceberg.lu>
2020-05-02 14:26:07 +0200

    refactoring

--- Gemfile
+++ Gemfile
@@ -2,12 +2,6 @@ source 'https://rubygems.org'
 
 ruby '2.6.3'
 
-group :sinatra do
-  gem 'sinatra'
-  gem 'sinatra-contrib'
-end
-
-gem 'better_errors'
-gem 'binding_of_caller'
+gem 'sinatra'
 gem 'dotenv'
 gem 'puma'
--- app.rb
+++ app.rb
@@ -1,19 +1,13 @@
 require 'sinatra'
 require 'open-uri'
 require 'dotenv'
-
 Dotenv.load
 
 URL = ENV['CUSTOM_URL'] || 'https://www.katsushikahokusai.org'
 
 get '/?*?' do
   url = URL.chomp('/') + request.path
-  dl(url)
-end
-
-private
-
-def dl(url)
   result = URI.parse(url).read
-  halt 200, { 'Content-Type' => result.content_type }, result
+  content_type result.content_type
+  result
 end
