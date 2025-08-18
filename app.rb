d5b1c818e05ea3a64bd8f62eb7673b9b149eaac7
schopenhauer <schopenhauer@users.noreply.github.com>
2025-08-18 10:32:53 +0200

    refactoring

--- app.rb
+++ app.rb
@@ -1,13 +1,33 @@
 require 'sinatra'
-require 'open-uri'
+require 'net/http'
+require 'uri'
 require 'dotenv'
 Dotenv.load
 
-URL = ENV['CUSTOM_URL'] || 'https://www.katsushikahokusai.org'
+CUSTOM_URL = ENV['CUSTOM_URL'] || 'https://www.katsushikahokusai.org'
 
-get '/?*?' do
-  url = URL.chomp('/') + request.path
-  result = URI.parse(url).read
-  content_type result.content_type
-  result
+helpers do
+  def mirror_request(path, query_string, headers)
+    url = URI.join(CUSTOM_URL.chomp('/'), path)
+    url.query = query_string unless query_string.nil? || query_string.empty?
+
+    req = Net::HTTP::Get.new(url)
+    # Forward main headers (except host)
+    headers.each do |k, v|
+      req[k] = v unless k.downcase == 'host'
+    end
+
+    Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == 'https') do |http|
+      http.request(req)
+    end
+  rescue => e
+    halt 502, "Mirroring failed: #{e.message}"
+  end
+end
+
+get '/*' do
+  response = mirror_request(request.path, request.query_string, request.env.select { |k, _| k.start_with?('HTTP_') })
+  content_type response.content_type
+  status response.code.to_i
+  response.body
 end
